a46550fd566e7139ade423118450832b18226dcc
gelutzu <gelutzu@yahoo.com>
2018-02-17 10:00:47 -0600

    added bonus

--- liri.js
+++ liri.js
@@ -22,6 +22,7 @@ function movie(title) {
 
     var i = 3;
     var movieName = "";
+    var fs = require("fs");
 
     if (!title) {
 
@@ -49,16 +50,58 @@ function movie(title) {
             console.log("The movie's year is: " + JSON.parse(body).Year);
             console.log("The movie's imdbRating is: " + JSON.parse(body).imdbRating);
             console.log("The movie's Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[1].Value);
-            console.log("The movie's was produced in: " + JSON.parse(body).Country);
+            console.log("The movie was produced in: " + JSON.parse(body).Country);
             console.log("The movie's language is: " + JSON.parse(body).Language);
             console.log("The movie's plot is: " + JSON.parse(body).Plot);
             console.log("The movie's actors are: " + JSON.parse(body).Actors);
+
+            fs.appendFile("log.txt", " \nThe movie's title is: " + JSON.parse(body).Title, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", " \nThe movie's year is: " + JSON.parse(body).Year, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", " \nThe movie's imdbRating is: " + JSON.parse(body).imdbRating, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", " \nThe movie's Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[1].Value, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", " \nThe movie was produced in: " + JSON.parse(body).Country, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", " \nThe movie's language is: " + JSON.parse(body).Language, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", " \nThe movie's plot is: " + JSON.parse(body).Plot, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+            fs.appendFile("log.txt", " \nThe movie's actors are: " + JSON.parse(body).Actors, function(err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
         }
     });
 }
 
 // Twitter function
 function twitterFunc() {
+    var fs = require("fs");
 
     for (var i = 0; i < 5; i++) {
         (function(i) {
@@ -77,6 +120,17 @@ function twitterFunc() {
                     //console.log(tweets);
                     console.log(tweets[i].text);
                     console.log(tweets[i].created_at);
+                    fs.appendFile("log.txt", "\n" + tweets[i].text, function(err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                    });
+                    fs.appendFile("log.txt", "\n" + tweets[i].created_at, function(err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                    });
+
                 }
             });
         })(i)
@@ -86,6 +140,7 @@ function twitterFunc() {
 // Spotify function
 function spotifyFunc(title) {
 
+    var fs = require("fs");
     var Spotify = require('node-spotify-api');
     var spotify = new Spotify({
         id: process.env.SPOTIFY_ID,
@@ -119,6 +174,27 @@ function spotifyFunc(title) {
         console.log("The name of the song: " + data.tracks.items[0].name);
         console.log("Preview link: " + data.tracks.items[0].preview_url);
         console.log("The album: " + data.tracks.items[0].album.name);
+
+        fs.appendFile("log.txt", " \nThe name of the song: " + data.tracks.items[0].name, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+        fs.appendFile("log.txt", " \nThe Artist : " + data.tracks.items[0].artists[0].name, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+        fs.appendFile("log.txt", " \nPreview link: " + data.tracks.items[0].preview_url, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+        fs.appendFile("log.txt", " \nThe album: " + data.tracks.items[0].album.name, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
     });
 }
 
@@ -131,11 +207,30 @@ function random() {
         if (error) {
             return console.log(error);
         }
+        console.log(data);
+        //var dataArr = data.split(",");
+        var dataArr = data.split("\n");
+        //var data2 = data1.split("\r");
+        //dataArr = data.split("\n");
+        var data0 = dataArr[0];
+        var data1 = dataArr[1];
+        var rowSplit1 = data0.split(",");
+        var rowSplit2 = data1.split(",");
 
-        var dataArr = data.split(",");
+        //console.log(rowSplit1);
+        //console.log(rowSplit2);
 
-        var arg1 = dataArr[0];
-        var arg2 = dataArr[1].split('"').join('');
+        if (Math.floor(Math.random() * 2) === 0) {
+            arg1 = rowSplit1[0];
+            arg2 = rowSplit1[1].split('"').join('');
+            console.log(arg1);
+            console.log(arg2);
+        } else {
+            arg1 = rowSplit2[0];
+            arg2 = rowSplit2[1].split('"').join('');
+            console.log(arg1);
+            console.log(arg2);
+        }
 
         switch (arg1) {
             case "my-tweets":
